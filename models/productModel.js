f6a4bb817c7243d05d96e399e1e429709ae277c8
!pro-grammer <124575915+msbasilisco@users.noreply.github.com>
2024-11-22 13:04:43 +0800

    feat(product): Add product model

---
+++ models/productModel.js
@@ -0,0 +1,76 @@
+module.exports = (sequelize, DataTypes) => {
+  const Product = sequelize.define('Product', {
+    productID: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    seller_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: 'Users', 
+        key: 'userID',
+      },
+      onUpdate: 'CASCADE',
+      onDelete: 'SET NULL',
+    },
+    product_Category: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: 'categories', 
+        key: 'categoryID',
+      },
+    },
+    prodVariation: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: 'variations',
+        key: 'variationID',
+      },
+    },
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    description: {
+      type: DataTypes.TEXT, 
+      allowNull: true,
+    },
+    prod_image: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    keyTags: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  }, {
+    tableName: 'products',
+    timestamps: true,       
+    paranoid: true,          
+  });
+
+ 
+  Product.associate = (models) => {
+    Product.belongsTo(models.User, {
+      foreignKey: 'seller_id',
+      as: 'seller',
+    });
+
+    Product.belongsTo(models.Category, {
+      foreignKey: 'product_Category',
+      as: 'category',
+    });
+
+    Product.belongsTo(models.Variation, {
+      foreignKey: 'prodVariation',
+      as: 'variation',
+    });
+  };
+
+  return Product;
+};
