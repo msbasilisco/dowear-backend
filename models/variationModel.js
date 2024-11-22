7aa8bbef3a13ec9c4bc0770518ca1ecc52815bfa
!pro-grammer <124575915+msbasilisco@users.noreply.github.com>
2024-11-22 14:54:07 +0800

    feat: Add variation model

---
+++ models/variationModel.js
@@ -0,0 +1,46 @@
+const { Sequelize, DataTypes } = require("sequelize");
+
+module.exports = (sequelize, DataTypes) => {
+  const Variation = sequelize.define('Variation', {
+    variationID: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    variation: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    price: {
+      type: DataTypes.FLOAT,
+      allowNull: false,
+      validate: {
+        min: 0,
+      },
+    },
+    quantity: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      validate: {
+        min: 0,
+      },
+    },
+  }, {
+    sequelize,
+    modelName: 'Variation',
+    tableName: 'variations',
+    timestamps: true,
+    paranoid: true,
+  });
+
+  
+  Variation.associate = (models) => {
+    Variation.belongsTo(models.Product, {
+      foreignKey: 'productID',  
+      as: 'product',
+    });
+  };
+
+  return Variation;
+};
