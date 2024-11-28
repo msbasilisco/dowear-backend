c58ffbc97be1d9c853f0f9fba58ca6c6cfb9f71d
!pro-grammer <124575915+msbasilisco@users.noreply.github.com>
2024-11-28 23:07:23 +0800

    feat(product): add routes for display products per category

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -3,7 +3,10 @@ const router = require('express').Router()
 const {protect } = require('../middleware/authMiddleware')
 
 router.post('/addProduct',protect, productController.createProduct)
+router.get('/products/tags/:tagName',productController.getProductsByTag)
+router.get('/products/category/:catID', productController.getAllProductsByCategory)
 router.put('/products/:id',productController.updateProduct)
 router.delete('/products/:id', productController.deleteProduct)
+
 
 module.exports = router;
