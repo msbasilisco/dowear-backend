acd0f10bc9cd9e836acbb59b020ba55731386380
!pro-grammer <124575915+msbasilisco@users.noreply.github.com>
2024-12-03 01:59:12 +0800

    feat: add product routes for latestProducts

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -6,6 +6,7 @@ router.post('/addProduct',protect, productController.createProduct)
 router.get('/products/tags/:tagName',productController.getProductsByTag)
 router.get('/products/category/:catID', productController.getAllProductsByCategory)
 router.get('/products/allProducts',productController.getAllProducts)
+router.get('/products/latest-Products', productController.getLatestProducts)
 router.put('/products/:id',productController.updateProduct)
 router.delete('/products/:id', productController.deleteProduct)
 
