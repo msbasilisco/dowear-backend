e1d14b5c3beff2767a8c9678d3a7dbd5dac95edf
!pro-grammer <124575915+msbasilisco@users.noreply.github.com>
2024-12-02 11:25:20 +0800

    feat: add product routes for getAllProducts

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -5,6 +5,7 @@ const {protect } = require('../middleware/authMiddleware')
 router.post('/addProduct',protect, productController.createProduct)
 router.get('/products/tags/:tagName',productController.getProductsByTag)
 router.get('/products/category/:catID', productController.getAllProductsByCategory)
+router.get('/products/allProducts',productController.getAllProducts)
 router.put('/products/:id',productController.updateProduct)
 router.delete('/products/:id', productController.deleteProduct)
 
