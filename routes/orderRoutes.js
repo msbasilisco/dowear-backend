ec433036ad9346c816b7bc7825b478f5d94ccb3a
Dee Bonachita <20101145@usc.edu.ph>
2024-12-03 07:45:46 +0800

    feat: added orderRoutes

---
+++ routes/orderRoutes.js
@@ -0,0 +1,15 @@
+const orderController = require('../controllers/orderController')
+const { authenticateToken } = require('../controllers/userController')
+const router = require('express').Router()
+
+// Create and manage orders
+router.post('/create', authenticateToken, orderController.createOrder)
+router.put('/:orderID/shipping', authenticateToken, orderController.updateShippingDetails)
+router.put('/:orderID/status', authenticateToken, orderController.updateOrderStatus)
+router.put('/:orderID/cancel', authenticateToken, orderController.cancelOrder)
+
+// Get order information
+router.get('/all', authenticateToken, orderController.getOrders)
+router.get('/:orderID', authenticateToken, orderController.getOrderDetails)
+
+module.exports = router
