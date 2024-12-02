98d29e853729600533a2cbf0ec2b4972af07a3e0
Psalmantha <ipongpsalm1203@gmail.com>
2024-12-02 23:52:07 +0800

    feat: add route for getCities

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,10 +1,11 @@
 const userController = require('../controllers/userController')
 const router = require('express').Router()
 
-router.post('/register',userController.register)
-router.post('/login',userController.login)
+router.post('/register', userController.register)
+router.post('/login', userController.login)
 router.get('/getUserProfile', userController.authenticateToken, userController.getUserProfile);
 router.get('/allUsers', userController.authenticateToken, userController.getAllUsers);
+router.get('/cities', userController.getCities);
 
 module.exports = router
 
