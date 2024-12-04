d7ba7c90ad46a3486131c9ea759a2807dd652bef
!pro-grammer <124575915+msbasilisco@users.noreply.github.com>
2024-12-04 11:57:00 +0800

    chore: change authentication file to protect for uniformity

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,10 +1,11 @@
 const userController = require('../controllers/userController')
 const router = require('express').Router()
+const { protect } = require('../middleware/authMiddleware');
 
 router.post('/register', userController.register)
 router.post('/login', userController.login)
-router.get('/getUserProfile', userController.authenticateToken, userController.getUserProfile);
-router.get('/allUsers', userController.authenticateToken, userController.getAllUsers);
+router.get('/getUserProfile', protect, userController.getUserProfile);
+router.get('/allUsers', protect, userController.getAllUsers);
 router.get('/cities', userController.getCities);
 
 module.exports = router
