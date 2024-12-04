1ab7a09719c46fa541079f077f9b9ff16e059a24
!pro-grammer <124575915+msbasilisco@users.noreply.github.com>
2024-12-04 11:56:12 +0800

    refactor: add authentication for product routes

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -7,8 +7,8 @@ router.get('/products/tags/:tagName',productController.getProductsByTag)
 router.get('/products/category/:catID', productController.getAllProductsByCategory)
 router.get('/products/allProducts',productController.getAllProducts)
 router.get('/products/latest-Products', productController.getLatestProducts)
-router.put('/products/:id',productController.updateProduct)
-router.delete('/products/:id', productController.deleteProduct)
+router.put('/products/:id',protect,productController.updateProduct)
+router.delete('/products/:id',protect, productController.deleteProduct)
 
 
 module.exports = router;
