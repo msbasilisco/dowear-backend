f59f2ca324a6a8fa29b7cb91540e8e03fc13b29f
!pro-grammer <124575915+msbasilisco@users.noreply.github.com>
2024-12-13 21:03:20 +0800

    chore: update create products routing

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,14 +1,17 @@
-const productController = require ('../controllers/productController')
-const router = require('express').Router()
-const {protect } = require('../middleware/authMiddleware')
+const express = require('express');
+const router = express.Router();
+const productController = require('../controllers/productController');
+const { protect } = require('../middleware/authMiddleware');
+const multer = require('multer');
+const storage = multer.memoryStorage(); // Use memory storage or disk storage
+const upload = multer({ storage: storage });
 
-router.post('/addProduct',protect, productController.createProduct)
-router.get('/products/tags/:tagName',productController.getProductsByTag)
-router.get('/products/category/:catID', productController.getAllProductsByCategory)
-router.get('/products/allProducts',productController.getAllProducts)
-router.get('/products/latest-Products', productController.getLatestProducts)
-router.put('/products/:id',protect,productController.updateProduct)
-router.delete('/products/:id',protect, productController.deleteProduct)
-
+router.post('/products/addProduct', protect, upload.single('product_image'), productController.createProduct);
+router.get('/products/tags/:tagName', productController.getProductsByTag);
+router.get('/products/category/:catID', productController.getAllProductsByCategory);
+router.get('/products/allProducts', productController.getAllProducts);
+router.get('/products/latest-Products', productController.getLatestProducts);
+router.put('/products/:id', protect, upload.single('product_image'), productController.updateProduct);
+router.delete('/products/:id', protect, productController.deleteProduct);
 
 module.exports = router;
